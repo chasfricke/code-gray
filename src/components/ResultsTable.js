7c98fc613d87d93d9087d95d9d05697c3790b105
Chas Fricke <chas.fricke@vangst.com>
2020-05-07 17:29:06 -0600

    API call working, basic table rendered

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,37 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 function ResultsTable() {
   const [restaurants, setRestaurants] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants",
+    const url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants';
+    fetch(url,
       {
         method: "GET",
-        cache: 'no-cache',
         headers: {
-          'API_Key': 'q3MNxtfep8Gt',
+          'Authorization': 'Api-Key q3MNxtfep8Gt',
         },
       })
       .then(res => res.json())
       .then(result => {
         setRestaurants(result)
-        setIsLoading(false)
+        setIsLoaded(true)
         console.log(result)
       },
         (error) => {
-          setIsLoading(true)
+          setIsLoaded(false)
           setError(error)
+          console.log(error)
         }
       )
   }, [])
 
-  return (
-    <div>
-      {/* {!isLoading && restaurants} */}
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <table>
+        <tbody>
+          {restaurants.map(item => (
+            <tr key={item.id}>
+              <td >
+                {item.name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default ResultsTable;
