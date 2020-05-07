322b175588bc3e76a7cb11aaebc347e6d27c3ca4
Chas Fricke <chas.fricke@vangst.com>
2020-05-07 14:18:21 -0600

    refactored css stylesheets into styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>Charter/Spectrum Front-End Code Challenge</div>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppContainer>
+        <Header>
+          <div>Charter/Spectrum Front-End Code Challenge</div>
+        </Header>
+      </AppContainer>
+    </>
   );
 }
 
