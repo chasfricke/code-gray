95b0a9f92dd3daa4a558c087abf461070959284b
Chas Fricke <chas.fricke@vangst.com>
2020-05-09 20:10:07 -0600

    moved logic into App.js and pulled ResultsTable out into it's own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components'
+import { TextField, Select, MenuItem, FormControl, InputLabel, Button } from '@material-ui/core/';
 import NavBar from './components/NavBar';
 import ResultsTable from './components/ResultsTable'
 
@@ -19,12 +20,131 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-function App() {
+const Body = styled.body`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`
+
+const FilterContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+background: #dadada;
+width: 100%;
+max-width: 800px;
+padding: 16px;
+margin: 6px 0 16px 0;
+border-radius: 4px;
+`
+
+const App = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState('');
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [stateSelect, setStateSelect] = useState('');
+  const [genreSelect, setGenreSelect] = useState('');
+
+  useEffect(() => {
+    const url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants';
+    fetch(url,
+      {
+        method: "GET",
+        headers: {
+          'Authorization': 'Api-Key q3MNxtfep8Gt',
+        },
+      })
+      .then(res => res.json())
+      .then(result => {
+        setRestaurants(sortFunction(result))
+        setFilteredRestaurants(result)
+        setIsLoaded(true)
+      },
+        (error) => {
+          setIsLoaded(false)
+          setError(error)
+          console.log(error)
+        }
+      )
+  }, [])
+
+  const sortFunction = (arr) => {
+    arr.sort((a, b) => {
+      if (a.name[0] > b.name[0]) {
+        return 1
+      } else if (a.name[0] < b.name[0]) {
+        return -1
+      } else return 0
+    })
+    return arr
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      filteredSearch()
+    }
+  }
+
+  const filteredSearch = () => {
+    setFilteredRestaurants(restaurants.filter(restaurant => {
+      return (
+        //Text Input
+        (restaurant.name.toLowerCase().includes(search.toLowerCase()) ||
+          restaurant.city.toLowerCase().includes(search.toLowerCase()) ||
+          restaurant.genre.toLowerCase().includes(search.toLowerCase())) &&
+        //State Select 
+        restaurant.state.toLowerCase().includes(stateSelect.toLowerCase()) &&
+        //Genre Select
+        restaurant.genre.toLowerCase().includes(genreSelect.toLowerCase())
+      )
+    }))
+  }
+
   return (
     <>
       <GlobalStyle />
       <NavBar />
-      <ResultsTable />
+      <Body>
+        <FilterContainer>
+          <TextField
+            label="Search"
+            variant="outlined"
+            onChange={e => setSearch(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <FormControl>
+            <InputLabel>Genre</InputLabel>
+            <Select
+              value={genreSelect}
+              onChange={e => setGenreSelect(e.target.value)}
+            >
+              <MenuItem value=""><em>All</em></MenuItem>
+              <MenuItem value="Steak">Steak</MenuItem>
+              <MenuItem value="Cafe">Cafe</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel>State</InputLabel>
+            <Select
+              value={stateSelect}
+              onChange={e => setStateSelect(e.target.value)}
+            >
+              <MenuItem value=""><em>All</em></MenuItem>
+              <MenuItem value="TX">Texas</MenuItem>
+              <MenuItem value="NY">New York</MenuItem>
+              <MenuItem value="FL">Florida</MenuItem>
+            </Select>
+          </FormControl>
+          <Button onClick={() => { filteredSearch() }} variant="contained" size="large">Search</Button>
+        </FilterContainer>
+        {isLoaded ? <ResultsTable data={filteredRestaurants} /> : <div>Searching for results...</div>}
+        {error && <div>{error}</div>}
+      </Body>
     </>
   );
 }
--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,135 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Select, MenuItem, FormControl, InputLabel, Button } from '@material-ui/core/';
+import React from 'react';
 
-function ResultsTable() {
-  const [restaurants, setRestaurants] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [error, setError] = useState(null);
-  const [search, setSearch] = useState('');
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const [stateSelect, setStateSelect] = useState('');
-  const [genreSelect, setGenreSelect] = useState('');
-
-  const sortFunction = (arr) => {
-    arr.sort((a, b) => {
-      if (a.name[0] > b.name[0]) {
-        return 1
-      } else if (a.name[0] < b.name[0]) {
-        return -1
-      } else return 0
-    })
-    return arr
-  }
-
-  useEffect(() => {
-    const url = 'https://code-challenge.spectrumtoolbox.com/api/restaurants';
-    fetch(url,
-      {
-        method: "GET",
-        headers: {
-          'Authorization': 'Api-Key q3MNxtfep8Gt',
-        },
-      })
-      .then(res => res.json())
-      .then(result => {
-        setRestaurants(sortFunction(result))
-        setFilteredRestaurants(result)
-        setIsLoaded(true)
-      },
-        (error) => {
-          setIsLoaded(false)
-          setError(error)
-          console.log(error)
-        }
-      )
-  }, [])
-
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      filteredSearch()
-    }
-  }
-
-  const filteredSearch = () => {
-    setFilteredRestaurants(restaurants.filter(restaurant => {
-      return (
-        //Text Input
-        (restaurant.name.toLowerCase().includes(search.toLowerCase()) ||
-          restaurant.city.toLowerCase().includes(search.toLowerCase()) ||
-          restaurant.genre.toLowerCase().includes(search.toLowerCase())) &&
-        //State Select 
-        restaurant.state.toLowerCase().includes(stateSelect.toLowerCase()) &&
-        //Genre Select
-        restaurant.genre.toLowerCase().includes(genreSelect.toLowerCase())
-      )
-    }))
-
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-
-    return (
-      <>
-        <TextField
-          label="Search"
-          variant="outlined"
-          onChange={e => setSearch(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <FormControl>
-          <InputLabel>Genre</InputLabel>
-          <Select
-            value={genreSelect}
-            onChange={e => setGenreSelect(e.target.value)}
-          >
-            <MenuItem value=""><em>All</em></MenuItem>
-            <MenuItem value="Steak">Steak</MenuItem>
-            <MenuItem value="Cafe">Cafe</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl>
-          <InputLabel>State</InputLabel>
-          <Select
-            value={stateSelect}
-            onChange={e => setStateSelect(e.target.value)}
-          >
-            <MenuItem value=""><em>All</em></MenuItem>
-            <MenuItem value="TX">Texas</MenuItem>
-            <MenuItem value="NY">New York</MenuItem>
-            <MenuItem value="FL">Florida</MenuItem>
-          </Select>
-        </FormControl>
-        <Button onClick={() => { filteredSearch() }} variant="contained" size="large">Search</Button>
-        {filteredRestaurants.length > 0 ? (
-          <table>
-            <tbody>
-              {filteredRestaurants.map(item => (
-                <tr key={item.id}>
-                  <td>{item.name}</td>
-                  <td>{item.city}</td>
-                  <td>{item.state}</td>
-                  <td>{item.telephone}</td>
-                  <td>{item.genre}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )
-          : (
-            <>
-              <div>No matches found were found.</div>
-              <div>Please refine your search.</div>
-            </>
-          )
-        }
-      </>
-    );
-  }
+const ResultsTable = (props) => {
+  return (
+    <>
+      <table>
+        <tbody>
+          {props.data.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.city}</td>
+              <td>{item.state}</td>
+              <td>{item.telephone}</td>
+              <td>{item.genre}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
+
 
 export default ResultsTable;
