bfbf6f7d757700d4c34b9c4b094833af84f4f2d5
Chas Fricke <chas.fricke@vangst.com>
2020-05-11 22:01:41 -0600

    improved PostsTable styling by adding icons and a better layout

--- src/components/PostsTable.js
+++ src/components/PostsTable.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import CallIcon from '@material-ui/icons/Call';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const StyledTD = styled.td`
+  width: 275px;
+  padding: 16px;
+  
+`
+
+const Name = styled.h3`
+  font-size: 28px;
+  font-weight: bold;
+  margin: 0;
+  font-style: italic;
+  color: rgba(0,0,0,0.76);
+`
+
+const IconRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: rgba(0,0,0,0.87);
+  p, a {
+    margin: 6px 0 6px 6px;
+  }
+`
+
+const StyledUL = styled.ul`
+  list-style: none;
+  color: rgba(0,0,0,0.87);
+  li {
+    margin: 2px 0;
+  }
+`
 
 const ResultsTable = (props) => {
 
   const getGenreList = (genreStr) => {
     const genreArr = genreStr.split(',')
     return (
-      <ul>
+      <StyledUL>
         {(genreArr.sort()).map(genre => (
           <li key={genre}>{genre}</li>
         ))}
-      </ul>
+      </StyledUL>
     )
   }
 
@@ -22,9 +57,12 @@ const ResultsTable = (props) => {
           <tbody>
             {props.data.map(item => (
               <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.city}, {item.state}</td>
-                <td>{item.telephone}</td>
+                <StyledTD>
+                  <Name>{item.name}</Name>
+                  <IconRow><CallIcon /><a href="tel:{item.telephone}">{item.telephone}</a></IconRow>
+                  <IconRow><LocationOnIcon /><p>{item.city}, {item.state}</p></IconRow>
+                </StyledTD>
+
                 <td>{getGenreList(item.genre)}</td>
               </tr>
             ))}
